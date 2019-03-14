4730d13125296e765218dd763103ca476643fab9
GBTC408014UR <GBTC408014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-14 17:49:28 -0400

    some css stuff

---
+++ src/main/java/com/example/demo/Springboot17Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot17Application.class, args);
+	}
+
+}
